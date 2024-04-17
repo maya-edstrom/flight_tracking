7d5b6156b248f91ebf81a3fb36a2ddb87878970a
Buckymaya-edstrom <medstrom@wisc.edu>
2024-04-17 18:50:39 +0000

    updates for lab 7

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@
 const PORT = 8000
 
 // Import Required Modules
+const {runQueries} = require('../server/database.js')
 const express = require("express")
 
 const app = express() 
@@ -133,7 +134,7 @@ app.get('/flights/:airport_code', async (request, response) => {
           console.log(" +++++++++ calling runQueries() +++++++++++++++")
           
           // Used for lab 7
-          // runQueries(json)
+          runQueries(json)
 
           console.log(" +++++++++ completed runQueries() +++++++++++++++")
 
